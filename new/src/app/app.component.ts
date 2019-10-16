fc5fbfd48d9e71a4731d96306f912ae108b61796
Vunf1 <epg.joaomaia@gmail.com>
2019-10-16 11:02:29 +0100

    app update

--- new/src/app/app.component.ts
+++ new/src/app/app.component.ts
@@ -1,11 +1,27 @@
 
-import {Component, OnDestroy} from "@angular/core";
-
+import {Component, OnDestroy, HostListener, OnInit} from "@angular/core";
+/**
+ * !;
+ *
+ */
 @Component({
   selector: "apr-root",
   styleUrls: ["./app.component.css", "./css/resume.min.css" ],
   templateUrl: "./app.component.html",
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   public title = "OverKill Project v.0.3.0";
+  screenWidth: any;
+  screenHeight: any;
+  public ngOnInit() {
+
+  }
+  @HostListener("window:resize", ["$event"])
+  public onResize(event?) {
+     this.screenHeight = window.innerHeight;
+     this.screenWidth = window.innerWidth;
+     if (this.screenWidth < 790) {
+       alert("oi");
+     }
+  }
 }
